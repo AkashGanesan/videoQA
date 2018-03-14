0a1a5aff6af9e1cfc9f9dc8e9d320806a0296fc7
Akash Ganesan <akaberto@gmail.com>
2018-03-14 18:25:37 -0400

    Main loop for generating graphs.

---
+++ src/python/main.py
@@ -0,0 +1,45 @@
+from subprocess import Popen, PIPE
+from time import sleep
+import os
+from easydict import EasyDict as edict
+import json
+
+scene_graph_exe = os.path.abspath("../../bin/sceneGraph.jar")
+start_str = "#start"
+
+
+class SceneGraph(object):
+
+    def __init__(self, scene_graph_exe, fw_file_name="tmpout", fr_file_name="tmpout", start_str="#start"):
+        self.init = 0
+        self.start_str = start_str
+        self.fw = open(fw_file_name, "wb")
+        self.fr = open(fr_file_name, "r")
+        self.p = Popen(["java", "-jar", scene_graph_exe], stdin = PIPE, stdout = self.fw, stderr = self.fw, bufsize = 1)
+
+
+    def start(self):
+        while(1):
+            init_str = self.fr.readline().strip()
+            if (init_str == self.start_str):
+                self.fr.readline()
+                break
+            sleep(1)
+        self.init = 1
+        print "Ready"
+    
+    def getVal(self, sentence):
+        if (self.init == 0):
+            print "Havent inited"
+            return None
+        self.p.stdin.write(sentence+'\n')
+        while(1):            
+            val = self.fr.readline()
+            if (val == ''):                
+                sleep(0.1)
+            else:
+                return  val
+        
+
+sg_obj = SceneGraph(scene_graph_exe)
+sg_obj.start()
