02c48b0b507f7db9ddbe58a1a0788035a1f69af5
Akash Ganesan <akaberto@gmail.com>
2018-04-05 16:42:11 -0400

    Added parameters here.

--- src/python/main.py
+++ src/python/main.py
@@ -1,13 +1,21 @@
 from pipeline import Pipeline
 
 
-dcapParams = {"api_key" : 'a59cd502-6bc9-4aac-a1af-642bec4fc71c',
-              "request_url" :"https://api.deepai.org/api/densecap"}
+dcapParams = {
+    "api_key" : 'a59cd502-6bc9-4aac-a1af-642bec4fc71c',
+    "request_url" :"https://api.deepai.org/api/densecap"}
 
-sceneGraphParams = {"fw_file_name" : "tmpout",
-                    "fr_file_name" : "tmpout",
-                    "scene_graph_exe" : os.path.abspath("../../bin/sceneGraph.jar"),
-                    "start_str" : "#start"}
+sceneGraphParams = {
+    "fw_file_name" : "tmpout",
+    "fr_file_name" : "tmpout",
+    "scene_graph_exe" : os.path.abspath("../../bin/sceneGraph.jar"),
+    "start_str" : "#start"}
+
+pipelineParams = {
+    "patterns" : ["*.jpg", "*.png"],
+    "image_path" : None,
+    "sceneGraphParams" : sceneGraphParams,
+    "dcapParams"  : dcapParams}
 
 
-pipeLine = Pipeline(sceneGraphParams, dcapParams)
+pipeLine = Pipeline(**pipelineParams)
